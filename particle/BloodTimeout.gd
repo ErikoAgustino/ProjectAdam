a1e501627bb9fe8ebc105a076767ba77341c3203
ErikoAgustino <1972041@maranatha.ac.id>
2023-05-04 14:44:09 +0700

    player

--- particle/BloodTimeout.gd
+++ particle/BloodTimeout.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 var maxtime = 2
 var positionStart = Vector2(0, 0)
+#var defaultColor = Color(255, 255, 255, 255)
+onready var particles = $Particles2D
 
 func _ready():
+#	particles.process_material.set("color", defaultColor)
 	var timer = Timer.new()
 	timer.wait_time = maxtime
 	timer.autostart = true
@@ -13,5 +16,5 @@ func _ready():
 func on_timeout():
 	self.queue_free()
 
-	
-
+func setColor(color: Color):
+	particles.process_material.set("color", color)
