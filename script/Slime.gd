a7c19d30600ec582ec47b807ad82c3f89ba633cb
ErikoAgustino <1972041@maranatha.ac.id>
2023-05-04 11:11:24 +0700

    generate tile fix

--- script/Slime.gd
+++ script/Slime.gd
@@ -59,6 +59,7 @@ func takesDamage(dmg, attackPosition):
 		get_parent().remove_child(self)
 		queue_free()
 
+
 func _on_Area2D_body_entered(body):
 	if(body.has_method("takesDamage")):
 		body.takesDamage(10, global_position)
